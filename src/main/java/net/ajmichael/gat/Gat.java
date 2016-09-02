4d53ad8630793079db3895dea9b05eba360834de
Adam Michael <adam@ajmichael.net>
2016-09-01 20:30:07 -0400

    Initial commit.

---
+++ src/main/java/net/ajmichael/gat/Gat.java
@@ -0,0 +1,8 @@
+package net.ajmichael.gat;
+
+public class Gat {
+
+  public static void main(String[] args) {
+    System.out.println("Starting gat");
+  }
+}
---
+++ src/main/java/net/ajmichael/gat/commands/Command.java
@@ -0,0 +1,4 @@
+package net.ajmichael.gat.commands;
+
+public interface Command {
+}
